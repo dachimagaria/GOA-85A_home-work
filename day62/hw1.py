0f78cb4b8c96e606906b7db7aa5884621b916d71
dachimagaria <dachiarkania023@gmail.com>
2026-05-26 17:12:20 +0400

    1

---
+++ day62/hw1.py
@@ -0,0 +1,19 @@
+# def create_phone_number(n):
+#      return '(' + str(n[0]) + str(n[1]) + str(n[2]) + ') ' + str(n[3]) + str(n[4]) + str(n[5]) + '-' + str(n[6]) + str(n[7]) + str(n[8]) + str(n[9])
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# def reverse_number(n):
+
+#     sign = 1
+
+#     if n < 0:
+#         sign = -1
+#         n = -n
+
+#     reversed_num = ""
+
+#     for i in str(n):
+#         reversed_num = i + reversed_num
+
+#     return int(reversed_num) * sign
