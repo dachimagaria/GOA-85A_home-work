deb30cafcd52746cc92817007611d6b5a1f78449
dachimagaria <dachiarkania023@gmail.com>
2026-08-06 22:28:09 +0400

    1

---
+++ day81/hw/script.js
@@ -0,0 +1,22 @@
+
+
+console.log("Dachi")
+
+
+console.log(16)
+
+console.log(175)
+
+/*
+7) მონაცემთა ტიპები:
+
+String - ტექსტი
+Number - რიცხვი
+Boolean - true ან false
+Undefined - მნიშვნელობა არ აქვს
+Null - ცარიელი მნიშვნელობა
+Object - ობიექტი
+Array - მასივი
+BigInt - ძალიან დიდი მთელი რიცხვები
+Symbol - უნიკალური მნიშვნელობა
+*/
---
+++ day82/cw/script.js
@@ -0,0 +1,56 @@
+
+
+
+console.log( 10 + 5)
+console.log( 20 - 9)
+console.log( 5 * 5)
+console.log( 15 / 6)
+console.log( 1 % 2)
+
+
+
+
+
+
+console.log("მე მქვია " + "დაჩი" + ". " + "ვსწავლობ პროგრამირებას.")
+
+console.log("5" + 10)
+console.log("5" - 10)
+console.log("5" * 10)
+console.log("5" / 10)
+console.log("5" % 10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("dachi".length);
+
+
+console.log("dachi".toUpperCase())
+console.log("dachi".toLowerCase())
+
+
+console.log("dachi".startsWith("g"))
+
+
+
+
+
+console.log("hello world javascript".trim())
+
+
+
+
+console.log("ჰიდროელექტროსადგური".slice(5, 12))
